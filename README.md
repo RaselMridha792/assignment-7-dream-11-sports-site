a945f96b716e22580fdf780bdb58e3f8d670e152
rasel mridha <raselmridha792@gmail.com>
2024-10-24 20:23:42 +0600

    ready all files. install and linking, dist deployment design in progress

---
+++ README.md
@@ -0,0 +1,2 @@
+Project name: BPL Dream 11
+live link surge: bpl-dream-11-A7.surge.sh
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>raselmridha</h1>
+      <button className="btn btn-primary">click me</button>
+    </>
+  )
+}
+
+export default App
